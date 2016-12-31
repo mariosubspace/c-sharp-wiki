8a075423fdfcf61e677e2d1bdce082f2d616403b
Mario Gutierrez <mario@kogocom.net>
2016-12-31 05:44:15 -0600

    Notes from Object Lifetime chapter.

--- notes-c-sharp.cs
+++ notes-c-sharp.cs
@@ -1105,3 +1105,13 @@ var res = from book in bookList select new {book.Title, book.Author};
 //     Max(), Min(), Average(), Sum().
 // * LINQ queries are shorthand for a bunch of extension methods (e.g.
 //   Where(Func<T> fn), Select(Func<T> fn), etc).
+
+
+/*** OBJECT LIFETIME STUFF ***/
+
+// * Finalize() is called when an object is garbage collected.
+//   * In C# you define this similar to a C++ destrutor.
+//   * This is used only in rare circumstances.
+// * IDisposable defines the Dispose().
+//   * Called by the user to manually release resources.
+// * Lazy<T> is a wrapper class that implements lazy initialization.
